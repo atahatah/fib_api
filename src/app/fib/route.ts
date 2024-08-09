7ac8e96a09cef71d002df5d7b1c8a0c0e81428c0
atahatah <ratdotl.hata@gmail.com>
2024-08-09 21:24:28 +0900

    レスポンスを数字として返す

--- src/app/fib/route.ts
+++ src/app/fib/route.ts
@@ -12,13 +12,15 @@ const fibonacci = (n: number): BigInt => {
 };
 
 export function GET(request: NextRequest): NextResponse {
-    const params = request.nextUrl.searchParams;
-    const query = params.get("n");
-    
-    if (!query) {
-        return NextResponse.json({ "error": "Query parameter 'n' is required" }, { status: 400 });
-    }
-    const n = parseInt(query);
-    const result = fibonacci(n).toString(); // Convert BigInt to string
-    return NextResponse.json({"result" : result});
+  const params = request.nextUrl.searchParams;
+  const query = params.get("n");
+
+  if (!query) {
+    return NextResponse.json({ "error": "Query parameter 'n' is required" }, { status: 400 });
+  }
+  const n = parseInt(query);
+
+  // BigIntはJSONに変換できないので、文字列に変換して返す
+  const result = fibonacci(n).toString();
+  return new NextResponse(`{"result": ${result}}`, { headers: { "Content-Type": "application/json" } });
 }
